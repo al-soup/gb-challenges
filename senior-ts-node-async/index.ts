b76a5fd9e4d56254430b552837c75b11016b6743
Alex Kräuchi <al@soup.one>
2025-08-10 10:46:48 +0200

    Solve challenge for async task (#2)
    * refactor: fetch ad impression counts concurrently
    
    * refactor: add error handling
    
    * docs: add notes for solution

--- senior-ts-node-async/index.ts
+++ senior-ts-node-async/index.ts
@@ -1,4 +1,3 @@
-// index.ts
 // --------------------
 // 1. TIMEOUT + MOCK DATA
 // --------------------
@@ -13,6 +12,13 @@ interface Impression {
   id: number;
   impressions: number;
 }
+
+type AdId = number;
+type ImpressionCount = number;
+type ImpressionSummary = {
+  totalImpressions: ImpressionCount;
+  executionTime: number;
+};
 
 const ads: Ad[] = [
   { id: 1, impressionId: 1 },
@@ -55,22 +61,53 @@ export const getAdImpression = (
 // --------------------
 // 3. TASK: getAllImpressions
 // --------------------
-export const getAllImpressions = async (): Promise<void> => {
+export const getAllImpressions = async (): Promise<ImpressionSummary> => {
   const adIds = await getAllAdsIds();
   const startTime = Date.now();
 
-  for (let adId of adIds) {
-    const ad = await getAdById(adId);
-    console.log("ad: ", ad);
-    const adImpressions = await getAdImpression(ad?.impressionId ?? 0);
-    console.log("adImpressions: ", adImpressions);
-  }
+  // Each add impression is stored in a map from which we later calculate the total impressions.
+  const adImpressions = new Map<AdId, ImpressionCount>();
+
+  const fetchAdImpressions = adIds.map(async (adId: AdId) => {
+    let adImpression: Impression | undefined;
+    try {
+      const ad = await getAdById(adId);
+      if (!ad) return;
+
+      adImpression = await getAdImpression(ad?.impressionId);
+    } catch (error) {
+      console.error(`Error fetching ad impression for adId ${adId}:`, error);
+
+      return;
+    }
+
+    if (adImpression) {
+      adImpressions.set(adId, adImpression.impressions);
+    }
+  });
+
+  // Run fetches for all ads and their impressions concurrently and write each result to the map as they are returned.
+  // Using the event loop in this way will handle multiple requests more efficiently with concurrency than waiting for each one sequentially.
+  // The minimum execution time of `getAdImpressions` is the greatest sum of the duration of `getAdById` + `getAdImpression` for a given adId.
+  // An alternative with parallel execution via worker threads is not suitable for this task as it is more I/O related than CPU extensive.
+  await Promise.all(fetchAdImpressions);
+
+  // Sum up the results
+  const totalImpressions = [...adImpressions.values()].reduce(
+    (sum, impressions) => sum + impressions,
+    0
+  );
 
   const endTime = Date.now();
-  console.log("execution time: ", endTime - startTime);
+  const executionTime = endTime - startTime;
+
+  return {
+    totalImpressions,
+    executionTime,
+  };
 };
 
 // Run to test:
 getAllImpressions()
-  .then((res) => console.log("Done:", res))
+  .then((res) => console.table(res))
   .catch((err) => console.error("Error: ", err));
